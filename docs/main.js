d019219bd52fc5a780f12d3721a283ca31f509da
rickkas7 <rickk@rickk.com>
2024-04-13 13:17:11 -0400

    checkpoint
    - in progress, not working

--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,117 @@
 $(document).ready(function() {
     console.log('loaded');
 
+    const thermistorResistance = [
+        {
+            temp: -50,
+            value: 329500,
+        },
+        {
+            temp: -40,
+            value: 188500,
+        },
+        {
+            temp: -30,
+            value: 111300,
+        },
+        {
+            temp: -20,
+            value: 67770,
+        },
+        {
+            temp: -10,
+            value: 42470,
+        },
+        {
+            temp: 0,
+            value: 27280,
+        },
+        {
+            temp: 10,
+            value: 17960,
+        },
+        {
+            temp: 20,
+            value: 12090,
+        },
+        {
+            temp: 25,
+            value: 10000,
+        },
+        {
+            temp: 30,
+            value: 8313,
+        },
+        {
+            temp: 40,
+            value: 5827,
+        },
+        {
+            temp: 50,
+            value: 4160,
+        },
+        {
+            temp: 60,
+            value: 3020,
+        },
+        {
+            temp: 70,
+            value: 2228,
+        },
+        {
+            temp: 80,
+            value: 1668,
+        },
+        {
+            temp: 85,
+            value: 1451,
+        },
+        {
+            temp: 90,
+            value: 1266,
+        },
+        {
+            temp: 100,
+            value: 973.1,
+        },
+        {
+            temp: 110,
+            value: 757.6,
+        },
+    ];
+
+    const resistanceForTemperature = function(degC) {
+        for(let ii = 0; ii < thermistorResistance.length - 1; ii++) {
+            if (degC >= thermistorResistance[ii].temp && degC < thermistorResistance[ii+1].temp) {
+                let calc = {degC};
+
+                calc.v1 = calc.degC - thermistorResistance[ii].temp;
+                calc.d1 = thermistorResistance[ii+1].temp - thermistorResistance[ii].temp;
+                calc.d2 = thermistorResistance[ii+1].value - thermistorResistance[ii].value;
+
+                calc.result = calc.v1 * calc.d2 / calc.d1 + thermistorResistance[ii].value;
+
+                // console.log('calc', calc);
+
+                return calc.result;
+            }
+        }
+        return NaN;
+    }
+
+    const updateFields = function(obj) {
+        for(const key in obj) {
+            let value = obj[key];
+
+            if (typeof value == 'number') {
+                value = Math.round(value * 100) / 100;
+            }
+
+            $('span[data-key="' + key + '"').text(value);
+        }
+    }
+
+
     const converter = new showdown.Converter({
         extensions: [
             showdownKatex({
@@ -14,13 +125,47 @@ $(document).ready(function() {
                 ],
             }),
         ],
+        tables: true,
     });
 
     fetch('main.md') 
         .then(response => response.text())
         .then(function(md) {
             $('#content').html(converter.makeHtml(md)); 
+
+
+
+            const calculateResistance = function() {
+                let param = {};
+                // vref can actually be anything, but this is just here to prove it's the case by
+                // changing it to other values. It's the voltage on REGN.
+                param.vref = 6.0;
+
+                param.tempLow = parseFloat($('#resCalcLow').val());
+                param.tempHigh = parseFloat($('#resCalcHigh').val());
+
+                param.rthcold = resistanceForTemperature(param.tempLow);
+                param.rthhot = resistanceForTemperature(param.tempHigh);
+
+                param.vltf = 0.735 * param.vref;
+                param.vhtf = 0.472 * param.vref;
+                param.vtco = 0.447 * param.vref;
+
+                param.rt2 = (param.vref * param.rthcold * param.rthhot * (1/param.vltf - 1/param.vtco)) / 
+                    (param.rthhot * (param.vref / param.vtco - 1) - param.rthcold * param.vref/param.vltf - 1);
+
+                param.rt1 = (param.vref/param.vltf - 1) / (1/param.rt2 + 1/param.rthcold);
+
+                console.log('resCalc', param);
+
+                updateFields(param);
+            };
+            calculateResistance();
+
+            $('#resCalcHigh,#resCalcLow').on('input', calculateResistance);
+        
+        
         });
 
-
+    
 });
--- docs/main.md
+++ docs/main.md
@@ -2,20 +2,103 @@
 
 *Calculator for bq24195 temperature sensor*
 
+
+## Resistor calculator
+
+Assuming a battery with a 103AT 10K thermistor, this section will calculate $RT1$ and $RT2$
+
+<table>
+    <tbody>
+        <tr>
+            <td>High cut-off temperature</td>
+            <td><input type="text" id="resCalcHigh" size="8" value="45" /></td>
+            <td>&deg;C</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Low cut-off temperature</td>
+            <td><input type="text" id="resCalcLow" size="8" value="0" /></td>
+            <td>&deg;C</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>High thermistor resistance</td>
+            <td><span data-key="rthhot"></span></td>
+            <td>&ohm;</td>
+            <td>For 103AT thermistor at temperature above</td>
+        </tr>
+        <tr>
+            <td>Low thermistor resistance</td>
+            <td><span data-key="rthcold"></span></td>
+            <td>&ohm;</td>
+            <td>For 103AT thermistor at temperature above</td>
+        </tr>
+        <tr>
+            <td>RT1</td>
+            <td><span data-key="rt1"></span></td>
+            <td>&ohm;</td>
+            <td>Calculated high-side resistor</td>
+        </tr>
+        <tr>
+            <td>RT2</td>
+            <td><span data-key="rt2"></span></td>
+            <td>&ohm;</td>
+            <td>Calculated low-side resistor</td>
+        </tr>
+    </tbody>
+</table>
+
+
+## Cut-off calculator
+
+
+## No temperature sensor
+
+
 ## Background
 
-Latex:
+The bq24195 PMIC supports a temperature sensor, typically part of the battery pack, to control charging based on temperature. From the datasheet:
 
-```latex
-x=\frac{ -b\pm\sqrt{ b^2-4ac } } {2a}
-```
+![](images/ts-network.png)
 
-ASCII math:
+The temperature sensor is a NTC (negative temperature coefficient) thermistor. For example, the [103AT](https://www.digikey.com/en/products/detail/semitec-usa-corp/103AT-2/16579059), a 10K NTC thermistor.
 
-```asciimath
-x = (-b +- sqrt(b^2-4ac)) / (2a)
-```
+![](images/thermistor.png)
 
-Inline test ~E=mc^2~
+When dealing with LiPo batteries, the charging is typically restricted to 0°C to 45°C. From the table and a little interpolation:
+
+| Temperature | Resistance |
+| :--- | :--- |
+| 0&deg;C | 27.28 K&ohm; |
+| 25&deg;C | 10 K&ohm; |
+| 40&deg;C | 5.827 K&ohm; |
+| 45&deg;C | 4.9935 K&ohm; (interpolated) |
+| 50&deg;C | 4.160 K&ohm; |
+
+There's this imposing formula in the datasheet for calculating $RT1$ and $RT2$:
+
+![](images/formula.png)
+
+And this figure:
+
+![](images/fig13.png)
+
+This isn't as bad as it looks when you break it down. Some of these values are constants:
+
+![](images/constants.png)
+
+The thing that's somewhat confusing with all of these voltages like $V_{VREF}$ is that you never deal with the actual voltages in the calculations! Note that the top side of the voltage divider formed by $RT1$, $RT2$ and the battery thermistor is $REGN$. As it turns out, $REGN$ isn't constant, it varies depending on $VBUS$! However, everything is either relative to, or a proportion of $REGN$ so you don't need to know the actual value of it.
+
+| Variable | Description |
+| :---  | :--- |
+| $RT1$ | High side of voltage divider |
+| $RT2$ | Low side of voltage divider, in parallel with battery thermistor |
+| $V_{VREF}$ | Reference voltage, a.k.a. $REGN$ |
+| $V_{REGN}$ | Reference voltage, a.k.a. $V_{VREF}$ |
+| $RTH_{HOT}$ | Resistance of thermistor at high temperature limit |
+| $RTH_{COLD}$ | Resistance of thermistor at low temperature limit |
+| $V_{LTF}$ | Cold temperature threshold, $0.735 \times V_{REGN}$ |
+| $V_{HTF}$ | High temperature threshold, $0.472 \times V_{REGN}$ |
+| $V_{TCO}$ | Cold temperature cut-off, $0.447 \times V_{REGN}$ |
 
 
